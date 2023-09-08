58a591a363a0c1c65e650b3fe52cfaf7d27250dd
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2023-09-07 22:04:32 -0600

    "feat(utils/rlManager): add displayMenu functionality"

--- src/utils/readlineManager/index.ts
+++ src/utils/readlineManager/index.ts
@@ -1,4 +1,5 @@
 import readline from 'readline'
+import { ACTIONS } from '../actions'
 
 const rlInterface = readline.createInterface({
   input: process.stdin,
@@ -9,6 +10,15 @@ export class RlManager {
   private rl: readline.Interface
   constructor(rl: readline.Interface) {
     this.rl = rl
+  }
+
+  displayMenu() {
+    const options = Object.keys(ACTIONS)
+
+    options.forEach((element, index) => {
+      let option = element.replace(/_/g, ' ')
+      console.log(`${index + 1} ${option}`)
+    })
   }
 
   askUser(question: string, callback: (answer: string) => void) {
